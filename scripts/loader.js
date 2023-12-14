d41d6ca7ab3a21f4377a2c3b7deba34ac02cbeb1
davideoguche <davideoguche@gmail.com>
2023-12-14 13:16:06 +0100

    Loading the right splash screen

--- scripts/loader.js
+++ scripts/loader.js
@@ -11,21 +11,37 @@ window.addEventListener("load", function() {
     });
 
     // start dynamic loading
+    // loading stage 1
     Modernizr.load([
         {
             // these files are always loaded
             load : [
                 "scripts/sizzle.js",
                 "scripts/dom.js",
-                "scripts/game.js",
-                "scripts/screen.splash.js",
-                "scripts/screen.main-menu.js"
-            ],
+                "scripts/game.js"
+            ]
+        }, {
+            test : Modernizr.standalone,
+            yep : "scripts/screen.splash.js",
+            nope : "scripts/screen.install.js",
             // called when all files have finished loading and executing
             complete : function() {
-                // show the first screen
-                jewel.game.showScreen("splash-screen");
+                if (Modernizr.standalone) {
+                    // show the first screen
+                    jewel.game.showScreen("splash-screen");
+                } else {
+                    jewel.game.showScreen("install-screen");
+                }
             }
         }
     ]);
+
+    // loading stage 2
+    if (Modernizr.standalone) {
+        Modernizr.load([
+            {
+                load : ["scripts/screen.main-menu.js"]
+            }
+        ]);
+    }
 }, false);
