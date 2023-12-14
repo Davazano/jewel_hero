1a0382e905c56b4f73476bcfd27f11adcda1279d
davideoguche <davideoguche@gmail.com>
2023-12-14 13:00:06 +0100

    Extended modernizr to test standalone for iOS and Safari

--- scripts/loader.js
+++ scripts/loader.js
@@ -5,6 +5,11 @@ var jewel = {
 
 // wait until main document is loaded
 window.addEventListener("load", function() {
+
+    Modernizr.addTest("standalone", function() {
+        return (window.navigator.standalone != false);
+    });
+
     // start dynamic loading
     Modernizr.load([
         {
