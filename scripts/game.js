548399306fd7673480d7292869dd04c17fbbc3e2
davideoguche <davideoguche@gmail.com>
2023-12-16 11:49:45 +0100

    Added setup function in game.js

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,14 @@ jewel.game = (function() {
     var dom = jewel.dom,
         $ = dom.$;
     
+    // setup function
+    function setup() {
+        //disable native touchmove behaviour to prevent overscroll
+        dom.bind(document, "touchmove", function(event) {
+            event.preventDefault();
+        });
+    }
+
     // hide the active screen (if any) and show the screen with the specified id
     function showScreen(screenId) {
         var activeScreen = $('#game .screen.active')[0],
