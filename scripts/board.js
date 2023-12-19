356957842d0375b75b9fff9e61f465c944898bdb
davideoguche <davideoguche@gmail.com>
2023-12-19 09:22:00 +0100

    Added the initialize and print functions to board.js

--- scripts/board.js
+++ scripts/board.js
@@ -1,8 +1,33 @@
 jewel.board = (function() {
+    var settings,
+        jewels,
+        cols,
+        rows,
+        baseScore,
+        numJewelTypes;
 
-    /* game functions go here */
+    function initialize() {
+        settings = jewel.settings;
+        numJewelTypes = settings.numJewelTypes;
+        baseScore = settings.baseScore;
+        cols = settings.cols;
+        rows = settings.rows;
+        fillBoard();
+    }
+
+    function print() {
+        var str = "";
+        for (var y = 0; y < rows; y++) {
+            for (var x = 0; x < cols; x++) {
+                str += getJewel(x, y) + " ";
+            }
+            str += "\r\n";
+        }
+        console.log(str);
+    }
 
     return {
-        /* exposed functions go here */
+        initialize : initialize,
+        print : print
     }
 })();
