56fdf139578523d02d264c18b52f67b065ed41b6
davideoguche <davideoguche@gmail.com>
2023-12-22 20:43:30 +0100

    Posting messages to the worker

--- scripts/board.worker-interface.js
+++ scripts/board.worker-interface.js
@@ -2,13 +2,28 @@
 jewel.board = (function() {
     var dom = jewel.dom,
         settings,
-        worker;
+        worker,
+        messageCount,
+        callbacks;
     
         function initialize(callback) {
             settings = jewel.settings;
             rows = settings.rows;
             cols = settings.cols;
 
+            messageCount = 0;
+            callbacks = [];
             worker = new Worker("scripts/board.worker.js");
+        }
+
+        function post(command, data, callback) {
+            callbacks[messageCount] = callback;
+
+            worker.postMessage({
+                id : messageCount,
+                command : command,
+                data : data
+            });
+            messageCount++;
         }
 })();
