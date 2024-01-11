ce235f2e499c87a79ec8e77d27c426bece0bf2cc
davideoguche <davideoguche@gmail.com>
2024-01-11 12:44:20 +0100

    Added the initialize function to input.js

--- scripts/input.js
+++ scripts/input.js
@@ -1,5 +1,27 @@
 jewel.input = (function() {
 
+    function initialize() {
+        inputHandlers = {};
+        var board = $("#game-screen .game-board")[0];
+
+        dom.bind(board, "mousedown", function(event) {
+            handleClick(event, "CLICK", event);
+        });
+        
+        dom.bind(board, "touchstart", function(event) {
+            handleClick(event, "TOUCH", event.targetTouches[0]);
+        });
+        
+        dom.bind(document, "keydown", function(event) {
+            var keyName = keys[event.keyCode];
+            if (keyName && settings.controls[keyName]) {
+                event.preventDefault();
+                trigger(settings.controls[keyName]);
+            }
+        });
+
+    }
+
     function bind(action, handler) {
         if (!inputHandlers[action]) {
             inputHandlers[action] = [];
