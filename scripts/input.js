3a9ce7349386168e5e666a647a345292e96fc902
davideoguche <davideoguche@gmail.com>
2024-01-11 12:42:57 +0100

    Added the bind function to input.js

--- scripts/input.js
+++ scripts/input.js
@@ -1,3 +1,14 @@
 jewel.input = (function() {
-    
+
+    function bind(action, handler) {
+        if (!inputHandlers[action]) {
+            inputHandlers[action] = [];
+        }
+        inputHandlers[action].push(handler);
+    }
+
+    return {
+        initialize : initialize,
+        bind : bind
+    };
 })();
